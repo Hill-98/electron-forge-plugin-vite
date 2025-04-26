697935cf8ccc9ddca303172ee83b1fbba061d6da
Zhong Lufan <lufanzhong@gmail.com>
2025-04-26 18:41:55 +0800

    chore(build): update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,52 @@
+import { rmSync as rm } from 'node:fs'
 import { builtinModules } from 'node:module'
+import { join } from 'node:path'
 import { defineConfig } from 'vite'
 import pkg from './package.json'
 
-export default defineConfig(({ mode }) => {
-  const isElectron = mode === 'electron'
+const dependencies = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.devDependencies),
+  ...Object.keys(pkg.peerDependencies),
+]
+const nodeBuiltinModules = [
+  ...builtinModules,
+  ...builtinModules.map((m) => `node:${m}`),
+]
 
-  return {
-    build: {
-      emptyOutDir: !isElectron,
-      outDir: 'dist',
-      target: ['node16'],
-      minify: false,
-      lib: {
-        entry: isElectron ? 'src/electron-protocol-helper.ts' : 'src/index.ts',
-        formats: isElectron ? ['es'] : ['cjs'],
-        fileName(format, entryName) {
-          return entryName.concat(format === 'es' ? '.mjs' : '.js')
-        },
-      },
-      reportCompressedSize: false,
-      rollupOptions: {
-        external: [
-          ...builtinModules,
-          ...builtinModules.map((v) => `node:${v}`),
-          ...Object.keys(pkg.dependencies),
-          ...Object.keys(pkg.peerDependencies),
-          'electron',
-        ],
+process.on('exit', async () => {
+  rm(join(import.meta.dirname, 'dist/electron-protocol-helper.js'))
+  rm(join(import.meta.dirname, 'dist/index.mjs'))
+})
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    target: ['node16'],
+    minify: false,
+    lib: {
+      entry: ['src/electron-protocol-helper.ts', 'src/index.ts'],
+      formats: ['cjs', 'es'],
+      fileName(format, entryName) {
+        return entryName.concat(format === 'es' ? '.mjs' : '.js')
       },
     },
-    define: {
-      'import.meta.env.VITE_MAIN_PUBLIC_DIR':
-        'import.meta.env.VITE_MAIN_PUBLIC_DIR',
-      'import.meta.env.VITE_RENDERER_OUT_DIR':
-        'import.meta.env.VITE_RENDERER_OUT_DIR',
+    reportCompressedSize: false,
+    rollupOptions: {
+      external(id) {
+        return (
+          nodeBuiltinModules.includes(id) ||
+          dependencies.some(
+            (dep) => id === dep || id.startsWith(dep.concat('/')),
+          )
+        )
+      },
     },
-  }
+  },
+  define: {
+    'import.meta.env.VITE_MAIN_PUBLIC_DIR':
+      'import.meta.env.VITE_MAIN_PUBLIC_DIR',
+    'import.meta.env.VITE_RENDERER_OUT_DIR':
+      'import.meta.env.VITE_RENDERER_OUT_DIR',
+  },
 })
